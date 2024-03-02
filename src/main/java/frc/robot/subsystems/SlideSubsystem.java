d71a48e536b62c88a3fd4afc3fd92af71e8910cc
ProCrafters <73192264+1ProCrafters@users.noreply.github.com>
2024-03-02 12:04:57 -0600

    Update SlideSubsystem.java

--- src/main/java/frc/robot/subsystems/SlideSubsystem.java
+++ src/main/java/frc/robot/subsystems/SlideSubsystem.java
@@ -13,7 +13,6 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.OperatorConstants;
 
-
 public class SlideSubsystem extends SubsystemBase {
   // MecanumSubsystem mec_subsystem = new MecanumSubsystem();
   OperatorConstants operatorConstants = new OperatorConstants();
@@ -22,21 +21,15 @@ public class SlideSubsystem extends SubsystemBase {
   DigitalInput firstPosSenseor = new DigitalInput(1);
   DigitalInput secondPosSenseor = new DigitalInput(2);
 
-
   private CANSparkMax slideMotor = new CANSparkMax(12, MotorType.kBrushless);
- 
-
-
   public RelativeEncoder encoderTopSlide;
   public RelativeEncoder encoderBottomSlide;
 
-  int slidePosition;
+  private boolean fromBottom = true;
 
-  public SlideSubsystem() {
+  public SlideSubsystem() { 
     slideMotor.restoreFactoryDefaults();
-
     encoderBottomSlide = slideMotor.getEncoder();
-
     encoderBottomSlide.setPosition(0);
   }
 
@@ -44,40 +37,50 @@ public class SlideSubsystem extends SubsystemBase {
     slideMotor.set(0);
   }
 
-  public void slide(double slideSpeed, int counter) {
-    System.out.println("Limit Switch 1: "+bottomLimitSwitch.get());
-    System.out.println("Limit Switch 2: "+secondPosSenseor.get());
-    System.out.println("Limit Switch 3: "+firstPosSenseor.get());
+  public void slide(double slideSpeed) {
+  }
 
-
-
-    if (counter == 1 && !firstPosSenseor.get())
-    {
-
-      System.out.println("POS 1 STOP ");
+  public void pos0() {
+    System.out.println("Limit Switch 1: " + bottomLimitSwitch.get());
+    if (bottomLimitSwitch.get()) {
+      System.out.println("BottomP ");
+      slideMotor.set(-1 * operatorConstants.slideSpeed);
+    } else if (!bottomLimitSwitch.get()){
       stop();
-
-      }
-      else if (counter == 2 && !secondPosSenseor.get())
-      {
-        System.out.println("POS 2 STOP ");
-        stop();
-      }else if (counter == 0 && !bottomLimitSwitch.get())
-      {
-        System.out.println("BottomP ");
-        stop();
-
-      }else
-      {
-       slideMotor.set(slideSpeed);
-      }
-      
-      
     }
-    
+  }
+
+  public void pos1() {
+    System.out.println("Limit Switch 3: " + firstPosSenseor.get());   
+    if (!bottomLimitSwitch.get()) {
+      fromBottom = true;
+    } else if (!secondPosSenseor.get()) {
+      fromBottom = false;
+    }
+    if (firstPosSenseor.get() && fromBottom) {
+      System.out.println("POS 1 STOP from bottom");
+      slideMotor.set(operatorConstants.slideSpeed);
+    } else if (firstPosSenseor.get() && !fromBottom) {
+      System.out.println("POS 1 STOP not from bottom");
+      slideMotor.set(-1 * operatorConstants.slideSpeed);
+    } else {
+    stop();
+    }
+  }
+
+  public void pos2() {
+    System.out.println("Limit Switch 2: " + secondPosSenseor.get());
+    if (secondPosSenseor.get()) {
+      System.out.println("POS 2 STOP ");
+      slideMotor.set(operatorConstants.slideSpeed);
+    } else {
+      stop();
+    }
+  }
 
   @Override
   public void periodic() {
+    // System.err.println(bottomLimitSwitch.get());
   }
 
   @Override
